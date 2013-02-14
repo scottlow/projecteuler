a669027dca9720adc258090afcf4cafb557e8ae4
scottlow <scouter32@gmail.com>
2013-02-14 15:35:00 -0800

    Completed Project Euler Problem 17

--- 17.py
+++ 17.py
@@ -1,24 +1,32 @@
-num = [3, 3, 5, 4, 4, 3, 5, 5, 4] # 1 - 9
-num1 = [3, 6, 6, 8, 8, 7, 7, 9, 8, 8] # 10 - 19
-num2 = [6, 6, 5, 5, 5, 7, 6, 6] # 20, 30, 40, etc.
+oneToNine = [3, 3, 5, 4, 4, 3, 5, 5, 4] # 1 - 9
+tenToNineteen = [3, 6, 6, 8, 8, 7, 7, 9, 8, 8] # 10 - 19
+twentyToNinety = [6, 6, 5, 5, 5, 7, 6, 6] # 20, 30, 40,..., 90
 
 hundred = 7
-oneHundredAnd = 10
+hundredAnd = 10
+oneThousand = 11
 
-oneToNineteen = sum(num) + sum(num1)
-twentyToNinetyNine = sum(num2)
+oneHundredTo999 = 0
 
-theRest = 0
+#1, 2, ... , 19
+oneToNineteen = sum(oneToNine) + sum(tenToNineteen)
 
-for i in range(1, len(num)):
-	twentyToNinetyNine += (num1[i] * len(num) + sum(num))
+# 20, 30, 40, ..., 90
+twentyToNinetyNine = sum(twentyToNinety)
 
-for i in range(1, len(num)):
-	theRest += hundred * num[i]
+# 20, 21, ... 99
+for i in range(0, len(twentyToNinety)):
+	twentyToNinetyNine += ((twentyToNinety[i] * 9) + sum(oneToNine))
 
-theRestButNot = oneToNineteen + twentyToNinetyNine
+oneToNinetyNine = oneToNineteen + twentyToNinetyNine
 
-for i in range(1, len(num)):
-	theRest += theRestButNot + 99 * oneHundredAnd
+#100, 200, ... , 900
+for i in range(0, len(oneToNine)):
+	oneHundredTo999 += oneToNine[i] + hundred
 
-print oneToNineteen + twentyToNinetyNine + theRest
+#101, 102,..., 999
+for i in range(0, len(oneToNine)):
+	oneHundredTo999 += (oneToNine[i] + hundredAnd) * 99 + oneToNinetyNine
+
+print oneToNinetyNine + oneHundredTo999 + oneThousand
+
